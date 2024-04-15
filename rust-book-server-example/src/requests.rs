29560f74a9b507c76d76d8116b4cf179f5620015
james sandy <jamessandy95@gmail.com>
2024-04-15 23:07:32 +0100

    refactor to move out function into files

---
+++ rust-book-server-example/src/requests.rs
@@ -0,0 +1,18 @@
+use std::{fs, thread, time::Duration};
+
+pub fn process_request(request: Vec<String>) -> String {
+    let (status_line, filename) = match request[0].as_ref() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    response
+}
