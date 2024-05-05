2b792e261d8eb0704552e92adc48857bd9e2ccba
james sandy <jamessandy95@gmail.com>
2024-05-05 19:27:22 +0100

    Rework request code - 1

--- rust-server/src/requests.rs
+++ rust-server/src/requests.rs
@@ -3,22 +3,25 @@ use std::{fs, sync::Arc, thread, time::Duration};
 use crate::Database;
 
 pub fn process_request(request: Vec<String>, db: Arc<impl Database>) -> String {
-    let (status_line, filename) = match request[0].as_ref() {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+    println!("Request is:");
+    for line in request.iter() {
+        println!("{:?}", line);
+    }
+
+    // No auth
+    let (status_line, body) = match request[0].as_ref() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", fs::read_to_string("hello.html").unwrap()),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
+            ("HTTP/1.1 200 OK", fs::read_to_string("hello.html").unwrap())
         }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+        _ => (
+            "HTTP/1.1 404 NOT FOUND",
+            fs::read_to_string("404.html").unwrap(),
+        ),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let value = db.get("test");
-    println!("Database value is {:}", value);
-    db.set("test".to_string(), value + "1");
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let length = body.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{body}");
     response
 }
