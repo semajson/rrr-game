99b7b338ffbe726c19ccfa595cc7c1668917f959
James Sandy <jamessandy95@gmail.com>
2024-06-18 23:30:48 +0100

    comment

--- rust-server/src/rrr_game/mod.rs
+++ rust-server/src/rrr_game/mod.rs
@@ -1,6 +1,6 @@
 // GAME_NAME:
 const GAME_NAME: &str = "rrr-game";
-const CHUNK_LENGTH: usize = 9;
+const CHUNK_LENGTH: usize = 9; // Currently, this is half like a global variable
 
 mod create;
 pub use create::create_game;
